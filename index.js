f0b2cbdd6fe0d8c6e4e40943b6178e38a417acdc
Jeisson Gonzalez <jeiglz13@gmail.com>
2021-12-30 19:28:04 -0600

    Node, Express and Mongo

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const dbConnection = require('./database/config');
+const cors = require('cors');
 require('dotenv').config();
 
 //Crear el servidor
 
  const app = express();
 
+//Base de datos
+
+dbConnection();
+
+//CORS
+app.use(cors());
+
+
  //Directorio publico
  app.use(express.static('public'));
+
+ //Lectura y parse del body
+ app.use(express.json());
 
  //Rutas
 
--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,33 @@
  */
 
 const {Router} = require('express');
+const {check} = require('express-validator');
 
 const router =  Router();
 
-router.get('/', (request, response)=>{
-    response.json({
-        ok: true
-    })
- });
+const {crearUsuario, loginUsuario, renovarToken} = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validarCampos');
+const { validarJWT } = require('../middlewares/validateJWT');
+
+
+router.post('/new', 
+[
+  check('name', 'El nombre es obligatorio').not().isEmpty(),
+  check('email', 'El email es obligatorio').isEmail(),
+  check('password', 'El password edebe ser de 6 caracteres o mas').isLength({min: 6}),
+  validarCampos
+],
+crearUsuario);
+
+router.post('/', 
+[
+  check('email', 'El email es obligatorio').isEmail(),
+  check('password', 'El password edebe ser de 6 caracteres o mas').isLength({min: 6}),
+  validarCampos
+],
+loginUsuario);
+
+router.get('/renew', validarJWT ,renovarToken);
+
 
  module.exports = router;
